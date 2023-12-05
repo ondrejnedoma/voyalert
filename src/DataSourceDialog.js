e1f6ddc5f3277fb83163cf5900b823099ea5ef08
ondrej <ondrejnedoma0@gmail.com>
2023-12-05 21:22:58 +0100

    Reworked add screen

---
+++ src/DataSourceDialog.js
@@ -0,0 +1,45 @@
+import {View} from 'react-native';
+import {Text, Icon, Dialog, Portal, useTheme} from 'react-native-paper';
+import React from 'react';
+
+export default function DataSourceDialog({visible, setVisible, content}) {
+  const theme = useTheme();
+  return (
+    <Portal>
+      <Dialog visible={visible} onDismiss={() => setVisible(false)}>
+        <Dialog.Title>{content.name}</Dialog.Title>
+        <Dialog.Content>
+          <Text style={{marginBottom: 12}} variant="bodyMedium">
+            {content.use}
+          </Text>
+          <View style={{gap: 4}}>
+            {content.pros.map(el => (
+              <View
+                key={el}
+                style={{flexDirection: 'row', alignItems: 'center'}}>
+                <Icon
+                  source="thumb-up"
+                  color={theme.colors.primary}
+                  size={20}
+                />
+                <Text style={{paddingHorizontal: 8}}>{el}</Text>
+              </View>
+            ))}
+            {content.cons.map(el => (
+              <View
+                key={el}
+                style={{flexDirection: 'row', alignItems: 'center'}}>
+                <Icon
+                  source="thumb-down"
+                  color={theme.colors.primary}
+                  size={20}
+                />
+                <Text style={{paddingHorizontal: 8}}>{el}</Text>
+              </View>
+            ))}
+          </View>
+        </Dialog.Content>
+      </Dialog>
+    </Portal>
+  );
+}
--- src/screens/AddScreen.js
+++ src/screens/AddScreen.js
@@ -1,30 +1,67 @@
-import { View, SafeAreaView } from 'react-native';
+import {View, SafeAreaView, TouchableWithoutFeedback} from 'react-native';
 import {
   Text,
   TextInput,
   Button,
   RadioButton,
-  Dialog, Portal,
+  Dialog,
+  Portal,
   Snackbar,
-  IconButton,
+  Icon,
+  useTheme,
 } from 'react-native-paper';
 import apiAdd from '../scripts/ApiAdd';
 import React from 'react';
+import DataSourceDialog from '../DataSourceDialog';
 
-export default function AddScreen({ navigation }) {
+export default function AddScreen({navigation}) {
+  const theme = useTheme();
   const [dataSource, setDataSource] = React.useState('');
   const [voyNumber, setVoyNumber] = React.useState('');
   const [loading, setLoading] = React.useState(false);
   const [showErrorSnackbar, setShowErrorSnackbar] = React.useState(false);
   const [errorSnackbarText, setErrorSnackbarText] = React.useState('');
   const dataSourceInfo = [
-    { value: "sz", name: "Správa Železnic (GRAPP)", use: 'This data source is applicable for all passenger trains on the Czech soil - grapp.spravazeleznic.cz', pros: ["Works with every carrier (ČD, RJ, LE, Arriva...)"], cons: ["Alerts only at railway stations with dispatchers"] },
-    { value: "cd", name: "České Dráhy", use: 'This data source is applicable for all ČD trains - grapp.spravazeleznic.cz', pros: ["Notifies at every stop"], cons: ["Only works with ČD trains"] },
+    {
+      value: 'sz',
+      name: 'Správa Železnic (GRAPP)',
+      use: 'This data source is applicable for all passenger trains on the Czech soil - grapp.spravazeleznic.cz',
+      pros: ['Works with every carrier (ČD, RJ, LE, Arriva...)'],
+      cons: [
+        'Alerts only at railway stations with dispatchers',
+        'Alerts can be around 2 minutes delayed',
+      ],
+      disabled: false,
+    },
+    {
+      value: 'cd',
+      name: 'České Dráhy (soon)',
+      use: 'This data source is applicable for all ČD trains - cd.cz',
+      pros: ['Alerts at every stop'],
+      cons: ['Only works with ČD trains'],
+      disabled: true,
+    },
+    {
+      value: 'idsok',
+      name: 'IDSOK (CestujOK) (soon)',
+      use: 'This data source is applicable for all vehicles in the IDSOK system',
+      pros: [],
+      cons: [],
+      disabled: true,
+    },
   ];
-  const [dataSourceDialogVisible, setDataSourceDialogVisible] = React.useState(false)
+  const [dataSourceDialogVisible, setDataSourceDialogVisible] =
+    React.useState(false);
+  const [dataSourceDialogContent, setDataSourceDialogContent] = React.useState({
+    value: '',
+    name: '',
+    use: '',
+    pros: [],
+    cons: [],
+  });
   const handleAdd = () => {
     setLoading(true);
-    apiAdd({ dataSource, voyNumber }).then(data => {
+    apiAdd({dataSource, voyNumber}).then(data => {
       setLoading(false);
       if (data.ok) {
         navigation.goBack();
@@ -34,45 +71,57 @@ export default function AddScreen({ navigation }) {
       }
     });
   };
+  const handleLongPressDataSource = dataSource => {
+    const content = dataSourceInfo.find(el => (el.value = dataSource));
+    setDataSourceDialogContent(content);
+    setDataSourceDialogVisible(true);
+  };
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ marginHorizontal: 24 }}>
-        <Text style={{ marginTop: 84 }} variant="displaySmall">
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{marginHorizontal: 24}}>
+        <Text style={{marginTop: 84}} variant="displaySmall">
           Add Voy...
         </Text>
-        <Text style={{ marginTop: 42, marginBottom: 8 }}>Data source:</Text>
+        <Text style={{marginTop: 42, marginBottom: 4}} variant="titleMedium">
+          Data source:
+        </Text>
+        <Text style={{marginBottom: 8}} variant="bodySmall">
+          Long press on data sources for details
+        </Text>
       </View>
       <RadioButton.Group
         onValueChange={newDataSource => setDataSource(newDataSource)}
         value={dataSource}>
-        {dataSourceInfo.map(el => <View style={{ flexDirection: "row", justifyContent: "space-between" }}><RadioButton.Item
-          key={el.value}
-          style={{ paddingHorizontal: 30, elevation: 0, flex: 1 }}
-          position="leading"
-          labelVariant="bodyMedium"
-          label={el.name}
-          value={el.value}
-        />
-          <IconButton icon="information" style={{ elevation: 10 }} /></View>
-        )}
+        {dataSourceInfo.map(el => (
+          <RadioButton.Item
+            key={el.value}
+            style={{paddingHorizontal: 30}}
+            position="leading"
+            labelVariant="bodyMedium"
+            label={el.name}
+            value={el.value}
+            disabled={el.disabled}
+            onLongPress={() => handleLongPressDataSource(el.value)}
+          />
+        ))}
       </RadioButton.Group>
-      <Portal>
-        <Dialog visible={dataSourceDialogVisible} onDismiss={() => setDataSourceDialogVisible(false)}>
-
-        </Dialog>
-      </Portal>
-      <View style={{ marginHorizontal: 24 }}>
+      <DataSourceDialog
+        visible={dataSourceDialogVisible}
+        setVisible={setDataSourceDialogVisible}
+        content={dataSourceDialogContent}
+      />
+      <View style={{marginHorizontal: 24}}>
         {dataSource ? (
           <>
             <TextInput
-              style={{ marginTop: 16 }}
+              style={{marginTop: 16}}
               mode="outlined"
               label="Number"
               value={voyNumber}
               onChangeText={text => setVoyNumber(text)}
             />
             <Button
-              style={{ alignSelf: 'flex-start', marginTop: 8 }}
+              style={{alignSelf: 'flex-start', marginTop: 8}}
               icon="check"
               mode="contained"
               disabled={loading}

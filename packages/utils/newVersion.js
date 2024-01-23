61eff0e2387c502fd95f2dfab563b09366ee296a
ondrej <ondrejnedoma0@gmail.com>
2024-01-23 18:39:45 +0100

    chore: create a script to automate new versions

---
+++ packages/utils/newVersion.js
@@ -0,0 +1,64 @@
+import * as fs from 'fs';
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
+import chalk from 'chalk';
+
+console.log(chalk.bold.bgMagentaBright(' Major/minor/patch? (M, m, p) '));
+const updateKind = prompt('> ');
+
+const rootPackageJson = JSON.parse(fs.readFileSync('../../package.json'));
+const oldVersion = rootPackageJson.version;
+const versionArray = oldVersion.split('.');
+
+switch (updateKind) {
+  case 'M':
+    versionArray[0] += 1;
+    break;
+  case 'm':
+    versionArray[1] += 1;
+    break;
+  case 'p':
+    versionArray[2] += 1;
+    break;
+  default:
+    console.error(chalk.bold.bgRedBright(' Invalid update type '));
+}
+
+const newVersion = versionArray.join('.');
+console.log(
+  chalk.bold.bgMagentaBright(
+    ` Updating versions (${oldVersion} => ${newVersion})... `,
+  ),
+);
+
+rootPackageJson.version = newVersion;
+fs.writeFileSync(
+  '../../package.json',
+  JSON.stringify(rootPackageJson, null, 2),
+);
+console.log(chalk.bold.bgMagentaBright(' Updated root package.json '));
+
+const allPackages = fs.readdirSync('../');
+
+for (const folder of allPackages) {
+  const folderPath = `../${folder}/package.json`;
+  const packageJson = fs.readFileSyncfolderPath;
+  packageJson.version = newVersion;
+  if (folder === 'app') {
+    const versionCode =
+      parseInt(versionArray[0]) * 10000 +
+      parseInt(versionArray[1]) * 100 +
+      parseInt(versionArray[2]);
+    packageJson.versionCode = versionCode;
+    const buildGradlePath = '../app/android/app/build.gradle';
+    const buildGradle = fs.readFileSync(buildGradlePath);
+    const newBuildGradle = buildGradle
+      .replace(/versionCode\s+\d+/, `versionCode ${versionCode}`)
+      .replace(/versionName\s+"[^"]+"/, `versionName "${newVersion}"`);
+    fs.writeFileSync(buildGradlePath, newBuildGradle);
+  }
+  fs.writeFileSync(folderPath, JSON.stringify(packageJson, null, 2));
+  console.log(
+    chalk.bold.bgMagentaBright(` Updated package.json for package ${folder} `),
+  );
+}
---
+++ packages/utils/removeModules.js
@@ -0,0 +1,21 @@
+// For safety reasons, this script does NOT use any other modules than the ones directly built into node to avoid issues with the removal of node_modules
+import * as fs from 'fs';
+
+// Delete node_modules for all individual packages
+const packages = fs.readdirSync('../');
+console.log(
+  `Deleting node_modules for ${packages.length} packages (${packages.join(
+    ', ',
+  )})...`,
+);
+for (const folder of packages) {
+  fs.rmSync(`../${folder}/node_modules`, {recursive: true, force: true});
+  console.log(`Deleted node_modules for ${folder}`);
+}
+console.log('\n');
+
+// Delete root node_modules
+console.log('Deleting root node_modules...');
+fs.rmSync('../../node_modules', {recursive: true, force: true});
+console.log('Deleted root node_modules');
+console.log('\n');
